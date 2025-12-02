496ca1402c882951b51a1de862de4f4465c877df
Your Name <you@example.com>
2025-12-02 22:36:37 +0100

    02.12.2025-22h36m37s

--- .config/hypr/wallpaper.sh
+++ .config/hypr/wallpaper.sh
@@ -1,4 +1,4 @@
 #if [ $1 -eq "Hyprland" ]; then
-swaybg -i $HOME/data/Wallpapers/wallpaper.active -m fill
+swaybg -i $HOME/data/Wallpapers/wallpaper.active -m fill #ln -sf ~/data/Wallpapers/{PATH TO IMG} ~/data/Wallpapers/wallpaper.active
 #else
 #fi
