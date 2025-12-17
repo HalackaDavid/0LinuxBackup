07cc8f692cb1c5dcb19896c0545341e7ca7f0232
Your Name <you@example.com>
2025-12-17 21:38:05 +0100

    17.12.2025-21h38m05s

--- .config/hypr/wallpaper.sh
+++ .config/hypr/wallpaper.sh
@@ -1,4 +1,3 @@
-#if [ $1 -eq "Hyprland" ]; then
+#Check path to wallpaper from hyprland.conf
+#if path to wallpaper.active exists equip it if not black screen
 swaybg -i $HOME/data/Wallpapers/wallpaper.active -m fill #ln -sf ~/data/Wallpapers/{PATH TO IMG} ~/data/Wallpapers/wallpaper.active
-#else
-#fi
