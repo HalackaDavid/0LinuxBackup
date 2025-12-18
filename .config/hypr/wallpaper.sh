45d613b9b71788319dc54e9e753d6445eea32f07
Your Name <you@example.com>
2025-12-18 22:02:14 +0100

    18.12.2025-22h02m14s

--- .config/hypr/wallpaper.sh
+++ .config/hypr/wallpaper.sh
@@ -1,27 +1,31 @@
 Wallpaper() {
     if [[ -f $HyprWallpaperPath/wallpaper.active ]]; then
-        swaybg -i $HyprWallpaperPath/wallpaper.active -m fill  >/dev/null 2>&1 </dev/null &
+        setsid swaybg -i $HyprWallpaperPath/wallpaper.active -m fill  >/dev/null 2>&1 </dev/null &
     else
-        swaybg -c "#000000" -m solid_color  >/dev/null 2>&1 </dev/null &
+        setsid swaybg -c "#000000" -m solid_color  >/dev/null 2>&1 </dev/null &
     fi
 }
 
 Kill() {
-    killall swaybg 2>/dev/null
+    pkill -x swaybg 2>/dev/null
 }
 
-if [[ $1 == "hypr_scrip" && ${#HyprWallpaperPath} > 0 ]]; then
+HyprWallpaperPath=""
+
+if [[ $1 == "hypr_script" && ${#HyprWallpaperPath} > 0 ]]; then
     Kill
     Wallpaper
     exit
-fi
+elif [[ ${#HyprWallpaperPath} > 0 ]]; then
 
-for i in $(ls $HyprWallpaperPath/0); do
-    echo "[ ] $i"
-done
-    echo "[x] Exit"
-echo -n ">> "
-read UserInput
-ln -sf $HyprWallpaperPath/0/$UserInput.png $HyprWallpaperPath/wallpaper.active 
-Kill
-Wallpaper
+    for i in $(ls $HyprWallpaperPath/0); do
+        echo "[ ] $i"
+    done
+        echo "[x] Exit"
+    echo -n ">> "
+    read UserInput
+    ln -sf "$HyprWallpaperPath/0/$UserInput".* "$HyprWallpaperPath/wallpaper.active"
+    Kill
+    Wallpaper
+    sleep 0.1 #Broken without
+fi
