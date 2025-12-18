c6216a5628acd23889bc01c75c85adf39de5d594
Your Name <you@example.com>
2025-12-18 22:39:11 +0100

    18.12.2025-22h39m11s

--- .config/hypr/wallpaper.sh
+++ .config/hypr/wallpaper.sh
@@ -10,22 +10,37 @@ Kill() {
     pkill -x swaybg 2>/dev/null
 }
 
-HyprWallpaperPath=""
-
 if [[ $1 == "hypr_script" && ${#HyprWallpaperPath} > 0 ]]; then
     Kill
     Wallpaper
     exit
 elif [[ ${#HyprWallpaperPath} > 0 ]]; then
-
+    found=0
     for i in $(ls $HyprWallpaperPath/0); do
-        echo "[ ] $i"
+        if [[ $HyprWallpaperPath/wallpaper.active -ef $HyprWallpaperPath/0/$i ]]; then
+            echo "[*] $i"
+            found=1
+        else
+            echo "[-] $i"
+        fi
     done
-        echo "[x] Exit"
+        if [[ $found == 0 ]]; then
+            echo "[*] Black"
+        else
+            echo "[-] Black"
+        fi
     echo -n ">> "
     read UserInput
+    [ "$UserInput" = "" ] && exit 0
     ln -sf "$HyprWallpaperPath/0/$UserInput".* "$HyprWallpaperPath/wallpaper.active"
     Kill
     Wallpaper
+    unset UserInput
+    unset i
+    unset found
     sleep 0.1 #Broken without
+else
+    echo "Wallpapar Path has 0 lenght."
+    echo "[Enter] to exit...."
+    read
 fi
