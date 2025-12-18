6a21c6d9d21c73fa1dd7f4fd070f77b6aa519ecf
Your Name <you@example.com>
2025-12-18 14:41:08 +0100

    18.12.2025-14h41m08s

--- .config/hypr/wallpaper.sh
+++ .config/hypr/wallpaper.sh
@@ -1,9 +1,7 @@
-#HyprWallpaperPath=$HOME/data/Wallpapers
-
 if [[ -f $HyprWallpaperPath/wallpaper.active ]]; then
     swaybg -i $HyprWallpaperPath/wallpaper.active -m fill
-else
-    swaybg -c "#000000" -m solid_color
+#else
+#    swaybg -c "#000000" -m solid_color
 fi
 
 #ln -sf ~/data/Wallpapers/{PATH TO IMG} ~/data/Wallpapers/wallpaper.active
