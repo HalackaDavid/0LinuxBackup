19f0950f8001e6aa7d4bae2d93f1cbf07f78ac85
Your Name <you@example.com>
2025-12-18 21:27:15 +0100

    18.12.2025-21h27m15s

--- .config/hypr/wallpaper.sh
+++ .config/hypr/wallpaper.sh
@@ -1,7 +1,27 @@
-if [[ -f $HyprWallpaperPath/wallpaper.active ]]; then
-    swaybg -i $HyprWallpaperPath/wallpaper.active -m fill
-#else
-#    swaybg -c "#000000" -m solid_color
+Wallpaper() {
+    if [[ -f $HyprWallpaperPath/wallpaper.active ]]; then
+        swaybg -i $HyprWallpaperPath/wallpaper.active -m fill  >/dev/null 2>&1 </dev/null &
+    else
+        swaybg -c "#000000" -m solid_color  >/dev/null 2>&1 </dev/null &
+    fi
+}
+
+Kill() {
+    killall swaybg 2>/dev/null
+}
+
+if [[ $1 == "hypr_scrip" && ${#HyprWallpaperPath} > 0 ]]; then
+    Kill
+    Wallpaper
+    exit
 fi
 
-#ln -sf ~/data/Wallpapers/{PATH TO IMG} ~/data/Wallpapers/wallpaper.active
+for i in $(ls $HyprWallpaperPath/0); do
+    echo "[ ] $i"
+done
+    echo "[x] Exit"
+echo -n ">> "
+read UserInput
+ln -sf $HyprWallpaperPath/0/$UserInput.png $HyprWallpaperPath/wallpaper.active 
+Kill
+Wallpaper
