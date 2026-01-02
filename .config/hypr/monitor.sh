7d5f984b9c62394d93f23251ef3d66a102c4c825
Your Name <you@example.com>
2026-01-02 21:54:58 +0100

    02.01.2026-21h54m58s

--- .config/hypr/monitor.sh
+++ .config/hypr/monitor.sh
@@ -1,9 +1,12 @@
+conf="$HyprConfigPath/monitor.conf"
+
 OLD() {
 
 internal="eDP-1"
-external=$(hyprctl monitors | grep "Monitor" | grep -v "disconnected" | awk '{print $2}' | grep -v "$internal" | head -n1)
+external=$(hyprctl monitors all | grep "Monitor" | grep -v "disconnected" | awk '{print $2}' | grep -v "$internal" | head -n1)
 
-conf="$HOME/.config/hypr/monitor.conf"
+#echo "" > $conf
+
 if [[ -n "$external" ]]; then
 cat <<EOF > "$conf"
 monitor=$external,1920x1080@60,0x0,1
@@ -16,7 +19,21 @@ EOF
 fi
 
 echo "$HyprConfigPath/monitor.conf"
-
 }
 
-OLD
+NEW() {
+    local internalName=$(hyprctl monitors all | grep "ID 0" | awk '{print $2}')
+    local externalName=$(hyprctl monitors all | grep "Monitor" | grep -v "disconnected" | grep -v "ID 0" | awk '{print $2}' | head -n1)
+
+    if [[ -n "$externalName" ]]; then
+        echo "monitor=$internalName,disable" > $conf
+    else
+        echo "" > $conf
+    fi
+}
+
+#OLD
+NEW
+
+hyprctl reload
+unset conf
