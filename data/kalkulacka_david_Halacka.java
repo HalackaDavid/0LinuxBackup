77c6e9fe6a121107de2bb0075399c0772d40dbb3
Your Name <you@example.com>
2026-01-14 09:30:32 +0100

    14.01.2026-09h30m32s

--- data/kalkulacka_david_Halacka.java
+++ data/kalkulacka_david_Halacka.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
+
 public class kalkulacka_david_halacka {
     public static void main(String[] args) {
         int x;
         int y;
         String operation; 
-        Scanner Input = new Scanner(System.In);
+        Scanner Input = new Scanner(System.in);
+        
+        System.out.print(":");
+        String inn = Input.nextLine();
+        String formated = inn.replace(",",".");
 
-
-        switch() {
+        switch(formated) {
             case "+":
 
             case "-":
