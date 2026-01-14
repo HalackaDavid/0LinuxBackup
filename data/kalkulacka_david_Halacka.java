24a67de8ecab39794e28869c4a57a4998fb8ef70
Your Name <you@example.com>
2026-01-14 13:56:26 +0000

    14.01.2026-14h56m26s

--- data/kalkulacka_david_Halacka.java
+++ data/kalkulacka_david_Halacka.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 public class kalkulacka_david_halacka {
     public static void main(String[] args) {
         Scanner Input = new Scanner(System.in);
-        
+        System.out.print("Vyber +, -, *, /\n");
+        String sep = Input.nextLine();
         System.out.print("Zadej x: ");
         String x = Input.nextLine();
         System.out.print("Zadej y: ");
         String y = Input.nextLine();
-        String sep = Input.nextLine();
 
-        System.out.print("Vyber +, -, *, /\n");
         switch(sep) {
             case "+":
                 System.out.print(Float.parseFloat(x.replace(",",".")) + Float.parseFloat(y.replace(",",".")));
