c5fbd5a314c7e320c499542b51c055f4f140452c
Your Name <you@example.com>
2026-01-14 09:57:45 +0100

    14.01.2026-09h57m45s

--- data/kalkulacka_david_Halacka.java
+++ data/kalkulacka_david_Halacka.java
@@ -2,24 +2,30 @@ import java.util.Scanner;
 
 public class kalkulacka_david_halacka {
     public static void main(String[] args) {
-        int x;
-        int y;
-        String operation; 
         Scanner Input = new Scanner(System.in);
         
-        System.out.print(":");
-        String inn = Input.nextLine();
-        String formated = inn.replace(",",".");
+        System.out.print("Zadej x: ");
+        String x = Input.nextLine();
+        System.out.print("Zadej y: ");
+        String y = Input.nextLine();
 
-        switch(formated) {
+        System.out.print("Vyber +, -, *, /\n");
+        switch(Input.nextLine()) {
             case "+":
-
+                System.out.print(Float.parseFloat(x.replace(",",".")) + Float.parseFloat(y.replace(",",".")));
+                break;
             case "-":
-
+                System.out.print(Float.parseFloat(x.replace(",",".")) - Float.parseFloat(y.replace(",",".")));
+                break;
             case "*":
-            
+                System.out.print(Float.parseFloat(x.replace(",",".")) * Float.parseFloat(y.replace(",",".")));
+                break;
             case "/":
-
+                System.out.print(Float.parseFloat(x.replace(",",".")) / Float.parseFloat(y.replace(",",".")));
+                break;
+            default:
+                System.out.print("debile něco si zkurvil");
+                break;
         }
     }
 }
