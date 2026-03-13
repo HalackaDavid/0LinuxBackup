6612d28cefdd321a898272dc9aeb1cf367576500
Your Name <you@example.com>
2026-03-13 11:28:57 +0100

    13.03.2026-11h28m57s

--- data/01/java/Halacka.java
+++ data/01/java/Halacka.java
@@ -2,51 +2,18 @@ import java.util.Scanner;
 
 public class Halacka {
     public static void main(String[] args) {
-        Scanner userInput = new Scanner(System.in);
-        String input;
-        int rounds = 0, 
-            addNumber = 0,
-            total = 0;
-
-        do {
-            System.out.print("číslo 0-100 nebo konec >> ");
-            input = userInput.nextLine();
-            
-            try {
-                addNumber = Integer.parseInt(input);
-
-                if (addNumber < 0) {
-                    continue;
-                } else if (addNumber > 100) {
-                    System.out.print("Maximální počet bodů na jedno kolo je 100!\n");
-                    continue;
-                }
-                total = addNumber + total;
-                rounds++;
-            } catch (NumberFormatException e) {
-                if (input.equals("konec")) {
-                    break;
-                } else {
-                    System.out.print("Neplatný vstup!\n");
-                }
-            }
-        } while(true);
+        Scanner scIn = new Scanner(System.in);
+        String[] name = new String[5];
         
-        if (total > -1 && rounds > 0) {
-            int avg = total / rounds;
-            String msg = "";
-
-            if (avg > 75) {
-                msg = "Výborný výkon";
-            } else if (avg > 39) {
-                msg = "Průměrný výkon";
-            } else {
-                msg = "Slabý výkon";
-            }
-            
-            System.out.print("===============================\ntotal : " + total + "\nrounds: " + rounds + "\navg   : " + avg + "\nmsg   : " + msg + "\n===============================\n");
+        for (int i = 0; i < name.length; i++) {
+            System.out.print(i+1 + "/" + name.length + " name>> ");
+            name[i] = scIn.nextLine();
         }
 
-        userInput.close();
+        for (String s:name) {
+            if (s.charAt(0) == 'P') {
+                System.out.print(s + "\n");
+            }
+        }
     }
 }
--- data/01/java/c.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main() {
-    char x[] = { (char)('0' + sizeof(long)), '\0' };   
-    puts(x);
-    return 0;
-}
