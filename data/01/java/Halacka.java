e937dfd0a8879368b63dbfeffcd2c9ac980d7c7c
Your Name <you@example.com>
2026-03-20 11:39:24 +0100

    20.03.2026-11h39m24s

--- data/01/java/Halacka.java
+++ data/01/java/Halacka.java
@@ -2,6 +2,36 @@ import java.util.Scanner;
 
 public class Halacka {
     public static void main(String[] args) {
-    
+        // 1 - vypis inventare
+        // 2 - vstup do mistnosti
+        // 3 - ukonceni programu
+        Scanner Input = new Scanner(System.in);
+        String[] inventory = {"1", "2", "1", "1", "30"};
+        int option = 0;
+
+        while (true) {
+            System.out.printf("Select options: \n[1] vypis inventare\n[2] vstup do mistnosti\n[3] ukonceni programu\n>>> ");
+            try {
+                option = Integer.parseInt(Input.nextLine());
+            } catch(NumberFormatException e) {
+                System.out.printf("pouze cisla\n\n");
+            }
+
+            switch (option) {
+                case 1:
+                    for (String s: inventory) {
+                        System.out.printf("%s\n", s);
+                    }
+                    System.out.printf("\n");
+                    break;
+                case 2:
+                    System.out.printf("2\n\n");
+                    break;
+                case 3:
+                    return;
+                default:
+                    System.out.printf("neni spravna moznost\n\n");
+            }
+        }
     }
 }
