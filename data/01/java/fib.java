16f56cb839b69d379a80ea550211bbb2a7772b90
Your Name <you@example.com>
2026-03-27 11:34:32 +0100

    27.03.2026-11h34m32s

---
+++ data/01/java/fib.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class fib {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 1;
+        int z = 0;
+        
+        for (int i = 0; i < 10; i++) {
+            z = x + y;
+        }
+    }
+}
