1f839bbb5acc48a85e11c8a503076bb310dc64f1
Your Name <you@example.com>
2026-04-10 19:13:27 +0200

    10.04.2026-19h13m27s

--- data/rnd/games/warframe/chroma.py
+++ data/rnd/games/warframe/chroma.py
@@ -1,10 +1,10 @@
 b_ar=0
-r_ar=5
+r_ar=2
 
 for i in range(r_ar+1):
     strg=100+77+99+55+(r_ar*15)
     ward=55*strg/100
-    hp_formula=1036+(255*b_ar)+(370*ward/100)
+    hp_formula=1036+(225*b_ar)+(370*ward/100)
     scorn=350*strg/100
     fury=275*strg/100
     vexh=1*5*strg/100/100
