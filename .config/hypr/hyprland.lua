462146b189d192d0a19225445fadb2386eeb9070
Dick Cumshot <FuckYou@seamen.cum>
2026-05-17 16:38:31 +0200

    17.05.2026-16h38m31s

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -30,25 +30,20 @@ hl.monitor({
     scale    = 1,
 })
 
---hl.monitor({
---    output   = "desc: LG Electronics LG ULTRAGEAR 0x0000B5F6",
---    mode     = "1920x1080@60",
---    position = "0x0",
---    scale    = 1,
---})
+hl.monitor({
+    output   = "desc: LG Electronics LG ULTRAGEAR 0x0000B5F6",
+    mode     = "1920x1080@60",
+    position = "0x0",
+    scale    = 1,
+})
 
 hl.monitor({
     output   = "desc: BOE NE160QDM-NYM",
+    mode     = "2560x1600@60",
+    position = "0x0",
+    scale    = 1.25,
     disabled = true
 })
-
---hl.monitor({
---    output   = "desc: BOE NE160QDM-NYM",
---    mode     = "2560x1600@60",
---    position = "0x0",
---    scale    = 1.25,
---    --disabled = true
---})
 
 ---------------
 --- STARTUP ---
